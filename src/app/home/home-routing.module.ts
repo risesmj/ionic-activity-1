e3cda0cee8d967725c2363d2fca7c4348e681b61
MJ <matheusjcunha@gmail.com>
2022-09-30 11:04:25 -0300

    commit initial

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,11 +6,26 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+      {
+        path: 'welcome',
+        loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomePageModule)
+      },
+      {
+        path: 'tabs',
+        loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule)
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
